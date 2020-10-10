88e65ce2c8c5d82e20502a4f12a7187bfef47476
Stewart Clelland <stewart@clelland.dev>
2020-10-10 00:52:15 -0400

    Further cleanup of vcpkg system

---
+++ vcpkg.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  vcpkg
+  GIT_REPOSITORY    https://github.com/microsoft/vcpkg.git
+)
+
+FetchContent_GetProperties(vcpkg)
+
+if(NOT vcpkg_POPULATED)
+  FetchContent_Populate(vcpkg)
+  execute_process(COMMAND ./${vcpkg_SOURCE_DIR}/bootstrap-vcpkg.sh)
+endif()
+
+set(CMAKE_TOOLCHAIN_FILE ${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake
+CACHE STRING "Vcpkg toolchain file")
